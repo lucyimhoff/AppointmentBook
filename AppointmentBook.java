a5b85bb08a5ce7b8b21a8df3ede4c15da1c3433f
lucyimhoff <lucyi4@nycstudents.net>
2024-11-04 14:29:45 +0000

    began

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -1,13 +1,21 @@
 public class AppointmentBook {
+    private int period;
+    private int minute;
+    private int startMinute;
+    private int duration;
+    private int startPeriod;
+    private int endPeriod;
     
 
     private boolean isMinuteFree(int period, int minute) {
+        
    
     private void reserveBlock(int period, int startMinute, int duration) {
 
     public int findFreeBlock(int period, int duration) {
  
-    public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
+     if(startPeriod = endPeriod {
 
 
 }
