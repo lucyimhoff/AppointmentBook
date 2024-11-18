f6b818ab41b6ba666d0a22d3df26401e4226d0d8
lucyimhoff <lucyi4@nycstudents.net>
2024-11-18 14:29:05 +0000

    workedonfindFreeBlock

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -14,14 +14,15 @@ public class AppointmentBook {
 
     public int findFreeBlock(int period, int duration) {
         int block = 0;
-        for(int i = 0; i < 60; i++)
-        if(isMinuteFree(period, i)){
+        for(int i = 0; i < 60; i++){
+        if(isMinuteFree(period, i))
+        {
             block++;
             if(block == duration) return i - duration + 1;
             else block = 0;
         }
+    }
         return -1;
-
     }
  
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
