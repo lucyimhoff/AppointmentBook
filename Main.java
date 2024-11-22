4f1293727c9353cc7859e04717992db45b25c976
lucyimhoff <lucyi4@nycstudents.net>
2024-11-22 14:28:57 +0000

    testing

--- Main.java
+++ Main.java
@@ -28,5 +28,17 @@ public static void main(String[] args) {
     b.printPeriod(4);
     System.out.println(b.makeAppointment(2, 4, 22));
     b.printPeriod(4);
+    System.out.println();
+    System.out.println();
+    System.out.println("Testing:");
+    schedule = new boolean[8][60];
+    for(int i = 25; i < 30; i++) schedule[1][i] = true;
+    for(int i = 0; i < 15; i++) schedule[2][i] = true;
+    for(int i = 41; i < 60; i++) schedule[2][i] = true;
+    for(int i = 5; i < 30; i++) schedule[3][i] = true;
+    for(int i = 44; i < 60; i++) schedule[3][i] = true;
+    AppointmentBook c = new AppointmentBook(schedule);
+
 }
+
 }
